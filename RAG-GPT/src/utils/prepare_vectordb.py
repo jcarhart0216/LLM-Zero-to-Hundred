259e7b0db5f98a605ae2a40a28fe76f038869216
jcarhart0216 <jcarhart@bamboohealth.com>
2024-09-08 15:36:34 -0400

    Update prepare_vectordb.py

--- RAG-GPT/src/utils/prepare_vectordb.py
+++ RAG-GPT/src/utils/prepare_vectordb.py
@@ -1,4 +1,5 @@
 from langchain_community.vectorstores import Chroma
+# from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
