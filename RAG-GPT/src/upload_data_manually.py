cc0eb63ddf84ef18783e3217d481520528d4f144
jcarhart0216 <jcarhart@bamboohealth.com>
2024-09-08 20:30:12 -0400

    Update upload_data_manually.py

--- RAG-GPT/src/upload_data_manually.py
+++ RAG-GPT/src/upload_data_manually.py
@@ -4,6 +4,8 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.utils.prepare_vectordb import PrepareVectorDB
 from src.utils.load_config import LoadConfig
+os.makedirs('/Users/jcarhart/Desktop/code_personal_use/LLM-Zero-to-Hundred/RAG-GPT/data/vectordb/uploaded/chroma', exist_ok=True)
+
 CONFIG = LoadConfig()
 
 
