2cfd67203fd4df62ffaad0776ab61e907982fbc0
jcarhart0216 <jcarhart@bamboohealth.com>
2024-09-10 21:05:33 -0400

    Update upload_file.py

--- RAG-GPT/src/utils/upload_file.py
+++ RAG-GPT/src/utils/upload_file.py
@@ -1,7 +1,7 @@
-from src.utils.prepare_vectordb import PrepareVectorDB
+from utils.prepare_vectordb import PrepareVectorDB
 from typing import List, Tuple
-from src.utils.load_config import LoadConfig
-from src.utils.summarizer import Summarizer
+from utils.load_config import LoadConfig
+from utils.summarizer import Summarizer
 
 APPCFG = LoadConfig()
 
