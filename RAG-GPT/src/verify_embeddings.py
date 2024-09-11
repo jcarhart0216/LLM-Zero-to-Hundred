5313368c4a5cf638ac5ec1564e9d192a2fbeeaf1
jcarhart0216 <jcarhart@bamboohealth.com>
2024-09-10 21:05:31 -0400

    Update verify_embeddings.py

--- RAG-GPT/src/verify_embeddings.py
+++ RAG-GPT/src/verify_embeddings.py
@@ -1,11 +1,12 @@
 from langchain_community.vectorstores import Chroma
-from utils.load_config import LoadConfig  # Import LoadConfig
+from utils.load_config import LoadConfig
 
 # Initialize APPCFG
 APPCFG = LoadConfig()
 
 
 def verify_embeddings():
+    # Ensure you are using the processed directory (preprocessed documents)
     vectordb = Chroma(persist_directory=APPCFG.persist_directory, embedding_function=APPCFG.embedding_model)
 
     # Perform a dummy search to retrieve some documents
